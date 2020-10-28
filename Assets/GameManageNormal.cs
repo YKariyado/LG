888a35eacc724cfb04f2c91e965c590be9d4635d
Yuta Kariyado <s1250192@u-aizu.ac.jp>
2020-10-28 22:56:13 +0900

    fixed buttons position

--- Assets/GameManageNormal.cs
+++ Assets/GameManageNormal.cs
@@ -74,7 +74,7 @@ public class GameManageNormal : MonoBehaviour
             {
                 for (int k = 0; k < n; k++)
                 {
-                    GameObject obj = Instantiate(dotPref, new Vector3(dotInterval * (-n / 2.0f + i), dotInterval * (-n / 2.0f + j), dotInterval * (-n / 2.0f + k)), Quaternion.identity,all.transform); // Generate dot prefabs from -n/2
+                    GameObject obj = Instantiate(dotPref, new Vector3(dotInterval * (-n / 2.0f + i), dotInterval * (-n / 2.0f + j), dotInterval * (-n / 2.0f + k)), Quaternion.identity, all.transform); // Generate dot prefabs from -n/2
                     obj.transform.localPosition = new Vector3(dotInterval * (-n / 2.0f + i)+0.5f, dotInterval * (-n / 2.0f + j) + 0.5f, dotInterval * (-n / 2.0f + k) + 0.5f);
                     //obj.transform.parent = all.transform;
                     obj.GetComponent<AudioSource>().volume = 1f / n;
@@ -105,6 +105,7 @@ public class GameManageNormal : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         Data.Instance.referer = "GoL";
 
         //rotate head :D
