76c3c69dbf4978be3fede4ba4f4af1bdfbc10e2e
Yuta Kariyado <s1250192@u-aizu.ac.jp>
2021-06-12 08:33:52 +0900

    added 3D infinite (actually finite because of the spec of machine) universe in GoL but it's not working fine. just reminder

---
+++ Assets/DotManageSparse.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DotManageSparse : PoolObj<DotManageSparse>
+{
+    //public int x, y, z;
+    //public int state = 0; //num of own
+    //public int neighbor = 0; //num of around
+
+    //public bool isAlive;
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    DotManageSparse.Pool(this);
+    //}
+
+    public override void Init()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public override void Sleep()
+    {
+        gameObject.SetActive(false);
+    }
+
+}
